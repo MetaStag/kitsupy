76cf3f961d94338a759ac80de19ac2891b3fd61b
MetaStag <70441461+MetaStag@users.noreply.github.com>
2021-06-10 14:05:09 +0530

    Add main file (pykitsu,py) and readme

---
+++ pykitsu.py
@@ -0,0 +1,88 @@
+# Pykitsu -> A simple api wrapper for the kitsu api written in python
+
+# MODULES
+from requests import get # To fetch api json response
+
+# the category (1st) argument for get_info() can be anything from this list
+get_info_supported_categories = ['anime', 'manga', 'episodes', 'chapters', 'categories',
+'category-favorites', 'anime-characters', 'manga-characters', 'characters', 'streamers',
+'streaming-links', 'users', 'user-roles', 'favorites', 'follows',
+'linked-accounts', 'blocks', 'profile-link-sites', 'profile-links', 'roles',
+'stats', 'library-entries', 'library-entry-logs', 'library-events', 'list-imports',
+'media-reaction-votes', 'media-reactions', 'review-likes', 'reviews', 'posts',
+'post-likes', 'post-follows', 'comments', 'comment-likes', 'anime-productions',
+'anime-staff', 'manga-staff', 'producers', 'people', 'castings',
+'group-action-logs', 'group-bans', 'group-invites', 'group-member-notes', 'group-members',
+'group-neighbors', 'group-permissions', 'group-reports', 'group-ticket-messages', 'group-tickets',
+'leader-chat-messages', 'reports', 'site-announcements']
+
+# Get explicit info on something from get_info_supported_categories (id required)
+def get_info(category, id):
+    category = category.lower()
+    if category not in get_info_supported_categories:
+        raise ValueError(f'Category {category} is not supported')
+
+    response = get(f'https://kitsu.io/api/edge/{category}/{id}').json()
+    try:
+        response =  response['data']['attributes']
+    except KeyError:
+        raise KeyError('Error parsing the json response, check the id')
+
+    return response
+
+# Get characters of anime/manga, or the anime/manga a characters features in
+def get_family(category, id):
+    category = category.lower()
+    if category in ['anime', 'manga']:
+        response = get(f'https://kitsu.io/api/edge/{category}/{id}/characters').json()
+        placeholder, placeholder2 = 'character', 'canonicalName'
+    elif category == 'character':
+        response = get(f'https://kitsu.io/api/edge/characters/{id}/media-characters').json()
+        placeholder, placeholder2 = 'media', 'canonicalTitle'
+    else:
+        raise ValueError(f'Invalid category {category}, it has to be one of anime, manga or character')
+
+    result = {}
+    try:
+        for i in response['data']:
+            new_response = get(i['relationships'][placeholder]['links']['related']).json()
+            result[new_response['data']['id']] = new_response['data']['attributes'][placeholder2]
+    except KeyError:
+        raise KeyError('Error parsing the json response, check the id')
+
+    return result
+
+# Get trending anime/manga
+def trending(category):
+    category = category.lower()
+    if category not in ['anime', 'manga']:
+        raise ValueError(f'Invalid argument {category}, it has to be either anime or manga')
+
+    response = get(f'https://kitsu.io/api/edge/trending/{category}').json()
+    result = {}
+    for i in response['data']:
+        result[i['id']] = i['attributes']['canonicalTitle']
+
+    return result
+
+# Search for anime, manga, characters, users
+def search(category, query):
+    category = category.lower()
+    query = query.replace(' ', '%20')
+    if category == 'characters':
+        placeholder, placeholder2 = 'name', 'canonicalName'
+    elif category == 'users':
+        placeholder = placeholder2 = 'name'
+    elif category in ['anime', 'manga']:
+        placeholder, placeholder2 = 'text', 'canonicalTitle'
+    else:
+        raise ValueError(f'Invalid category {category}, it has to be one of anime, manga, characters or users')
+
+    response = get(f'https://kitsu.io/api/edge/{category}?filter[{placeholder}]={query}').json()
+
+    result = {}
+    result['count'] = response['meta']['count']
+    for i in response['data']:
+        result[i['id']] = i['attributes'][placeholder2]
+
+    return result
---
+++ readme.md
@@ -0,0 +1,32 @@
+# Pykitsu
+
+Pykitsu is a simple api wrapper for the [kitsu](https://kitsu.io/explore/anime) api written in python. It should work without any problems on any reasonably recent version of python.
+
+### Installation
+Pykitsu can be installed from pypi
+```bash
+$ pip install pykitsu
+```
+
+### Dependencies
+This module requires only one external module, `requests`, which should be installed by-default with python. In case it's not, you can install it with pip:
+```bash
+$ pip install requests
+```
+
+### Example
+```py
+>>> import pykitsu # import the module
+>>> pykitsu.search('anime', 'bakemonogatari')
+{'count': 2, '3919': 'Bakemonogatari', '4800': 'Bakemonogatari Recap'}
+>>> # A dictionary is returned, from where we can get the id of the anime
+>>> pykitsu.get_info('anime', 3919) # Taking the id from the above dictionary
+{'createdAt': '2013-02-20T17:00:18.998Z', 'updatedAt': '2021-06-10T06:00:15.132Z', 'slug': 'bakemonogatari', 'synopsis': '#long description', 'coverImageTopOffset': 20, 'titles': {'en': 'Bakemonogatari', 'en_jp': 'Bakemonogatari', 'ja_jp': '化物語'}, 'canonicalTitle': 'Bakemonogatari', 'abbreviatedTitles': ['Ghostory']}
+>>> # Note that a lot more data is returned in the dictionary, but for the sake of keeping it simple, the dictionary has been truncated here
+```
+
+### Docs
+The documentation can be found [here]()
+
+### License
+This project can been licensed under the [MIT](https://github.com/MetaStag/Pykitsu/blob/main/LICENSE) license.
